de22c3526c7fe297595cbc6e260cee083035fb67
git <xiaopolin@mylvzuan.com>
2019-11-25 16:43:43 +0800

    update @ImportResource secquence

--- dubbo-provider/src/main/java/com/xpl/App.java
+++ dubbo-provider/src/main/java/com/xpl/App.java
@@ -7,8 +7,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@MapperScan(basePackages = "com.xpl.dao")
 @ImportResource(value = {"classpath:com/xpl/framework/config/spring-dubbo.xml"})
+@MapperScan(basePackages = "com.xpl.dao")
 @EnableTransactionManagement
 @SpringBootApplication
 public class App {
